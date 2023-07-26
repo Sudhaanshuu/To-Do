f4db19afc75beac63cf40efc7e9b81156afba354
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2023-07-26 20:57:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
+
+function renderTodos() {
+  listElement.innerHTML = "";
+
+  for (todo of todos) {
+    var todoElement = document.createElement("li");
+    var todoText = document.createTextNode(todo);
+
+    var linkElement = document.createElement("a");
+
+    linkElement.setAttribute("href", "#");
+
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute("onclick", "deleteTodo(" + pos + ")");
+
+    var linkText = document.createTextNode("done");
+
+    linkElement.appendChild(linkText);
+
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
+
+function addTodo() {
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = "";
+  renderTodos();
+  saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
+  renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("list_todos", JSON.stringify(todos));
+}
